23d5d6b676cf20046495ff7c8aa8d762563be256
aycodem <azeezbabalola10@gmail.com>
2023-10-26 12:56:37 +0100

    'the_first_commit'

---
+++ variable.java
@@ -0,0 +1,15 @@
+public class main{
+    public static void main(String[] args){
+        byte number1 = 32; //1 bytes
+        short number2 = 12; // 2 bytes
+        int number3 = 54; // 4 bytes
+        long number4 = 74L; // 8 bytes
+        float number5 = 77.42F; // 4 bytes
+        double number6 = 2_484_044; // 8 bytes
+        char letter = 'A'; // 2 bytes
+        boolean isEligible =false; // 1 bytes
+
+        System.out.println(letter);
+
+    }
+}
